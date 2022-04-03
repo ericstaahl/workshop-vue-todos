22e6b6ffc6844e10ec5fe26968200e52e9b224dd
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-04-03 10:34:06 +0200

    Fixed code indentation.

--- app.js
+++ app.js
@@ -21,28 +21,28 @@ Vue.createApp({
             const indexOfTodo = this.todos.findIndex(_todo => _todo.id === todo.id)
             this.todos.splice(indexOfTodo, 1)
         }
-},
-computed: {
-    unfinishedTodosLeft() {
-        let unfinishedTodosLeft = 0;
-        this.todos.forEach(todo => {
-            if(todo.completed === false) {
-                unfinishedTodosLeft++
-            }
-        })
-        return unfinishedTodosLeft
     },
-    unfinishedTodos() {
-        return this.todos.filter(todo => todo.completed === false)
-    },
-    finishedTodos() {
-        return this.todos.filter(todo => todo.completed === true)
-    },
-    isEmpty() {
-        const numberOfTodos = this.todos.length
-        if (numberOfTodos > 0) {
-            return true;
-        } else return false;
+    computed: {
+        unfinishedTodosLeft() {
+            let unfinishedTodosLeft = 0;
+            this.todos.forEach(todo => {
+                if (todo.completed === false) {
+                    unfinishedTodosLeft++
+                }
+            })
+            return unfinishedTodosLeft
+        },
+        unfinishedTodos() {
+            return this.todos.filter(todo => todo.completed === false)
+        },
+        finishedTodos() {
+            return this.todos.filter(todo => todo.completed === true)
+        },
+        isEmpty() {
+            const numberOfTodos = this.todos.length
+            if (numberOfTodos > 0) {
+                return true;
+            } else return false;
+        }
     }
-}
 }).mount('#app')
